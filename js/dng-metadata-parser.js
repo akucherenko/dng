b28c357cdd11f1abcfc1bbee09e3b6b599df2c5f
Andrey Kucherenko <ku4er.prg@gmail.com>
2013-09-03 23:36:30 +0300

    Just playing with bytes...

---
+++ js/dng-metadata-parser.js
@@ -0,0 +1,3 @@
+function DngMetadataParser() {
+    return this;
+}
--- js/file-collector.js
+++ js/file-collector.js
@@ -1,4 +1,5 @@
 function FileCollector() {
+    this.parsers = {};
     return this;
 }
 FileCollector.prototype.newObserver = function () {
@@ -9,7 +10,31 @@ FileCollector.prototype.notify = function (file) {
 
     if (!file.type || file.type != "image/x-adobe-dng") {
         console.error("Invalid file type.");
+        return;
     } else {
         console.debug("Got DNG file.");
     }
+
+    if (file.type in this.parsers) {
+        console.debug("Starting parser...");
+
+        var reader = new FileReader();
+
+        reader.onloadend = function (event) {
+            var byteOrderBuff = event.originalTarget.result.slice(0,2),
+                byteOrder = new DataView(byteOrderBuff);
+            if (byteOrder.getInt16(0) == 0x4949) {
+                console.debug("The file has little-endian byte order.");
+            } else if (byteOrder.getInt16(0) == 0x4d4d) {
+                console.debug("The file has big-endian byte order.");
+            } else {
+                console.error("Invalid file format.");
+            }
+        };
+
+        reader.readAsArrayBuffer(file);
+    }
+}
+FileCollector.prototype.setMetadataParser = function (type, parser) {
+    this.parsers[type] = parser;
 }
